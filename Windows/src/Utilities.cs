kqarryzada/NeoDefaults

commit 5aa3d3565da670136ccef0b31113c95dd15867e9

Automatic install search looks for tf/ folder
Main.RequestFilePath() requests the 'hl2.exe' file since the navigation
menu requires providing a file instead of a folder. Even though this was
being done in Utilities.SearchForTF2Install() as well, it really wasn't
necessary. For clarity's sake, SearchForTF2Install() now just searches
for the tf/ folder instead of the exe.

Windows/src/Utilities.cs
@@ -24,9 +24,9 @@ namespace NeoDefaults_Installer {
 
         // TF-path related parameters. You must use String.Format() and specify a drive name to use these.
         private readonly String[] defaultInstallLocations = {
-            @"{0}Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\hl2.exe",
-            @"{0}Steam\SteamApps\common\Team Fortress 2\hl2.exe",
-            @"{0}SteamLibrary\SteamApps\common\Team Fortress 2\hl2.exe",
+            @"{0}Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\tf",
+            @"{0}Steam\SteamApps\common\Team Fortress 2\tf",
+            @"{0}SteamLibrary\SteamApps\common\Team Fortress 2\tf",
         };
 
         // The name of the folder inside of tf/cfg/ that holds the installer config files.
@@ -101,7 +101,7 @@ namespace NeoDefaults_Installer {
                 }
             });
             // Will hold the location to a TF2 install, if not null.
-            String hl2Path = null;
+            String installPath = null;
 
             // Placeholder for paths being checked. Allocated here for use by debug messages.
             String path = null;
@@ -125,8 +125,8 @@ namespace NeoDefaults_Installer {
                             path = String.Format(_path, drive.Name);
 
                             log.Write("Checking if the path exists: " + path);
-                            if (File.Exists(path)) {
-                                hl2Path = path;
+                            if (Directory.Exists(path)) {
+                                installPath = path;
                                 log.Write();
                                 log.Write("Found install at: " + path);
 
@@ -147,10 +147,9 @@ namespace NeoDefaults_Installer {
                 }
             });
 
-            if (hl2Path != null) {
-                // Strip "hl2.exe" from the path to obtain the folder path
-                var TeamFortressPath = CanonicalizePath(Path.GetDirectoryName(hl2Path));
-                tfPath = Path.Combine(TeamFortressPath, "tf");
+            if (installPath != null) {
+                // If the operation fails, the value of tfPath will still remain null.
+                tfPath = CanonicalizePath(Path.GetDirectoryName(installPath));
             }
         }
 

commit d92cbd66a177b12d9d5e8546eff82407bb4582a3

Fix issue with improper tfPath value
Fixed a regression introduced in
5aa3d3565da670136ccef0b31113c95dd15867e9 that caused 'tf' to be dropped
from the end of the path.

Windows/src/Utilities.cs
@@ -82,6 +82,11 @@ namespace NeoDefaults_Installer {
          * This method obtains all the drives available on the system, then searches for a TF2 path
          * under each drive until either a valid install is found, or until all possibilities have
          * been exhausted.
+         *
+         * If a potential install is found, it will be stored in the 'tfPath' private member
+         * variable, but will not officially be recognized until it is handled in Main (specifically
+         * in Main.PreparePathPanel()) since there are UI elements that depend on whether or not the
+         * install was automatically found.
          */
         private async void SearchForTF2Install() {
             // Obtain the list of drive names on the system.
@@ -146,8 +151,14 @@ namespace NeoDefaults_Installer {
             });
 
             if (installPath != null) {
-                // If the operation fails, the value of tfPath will still remain null.
-                tfPath = CanonicalizePath(Path.GetDirectoryName(installPath));
+                try {
+                    String parent = Path.GetDirectoryName(installPath);
+                    tfPath = Path.Combine(parent, "tf");
+                }
+                catch (Exception e) {
+                    log.WriteErr("Failed to set the TF2 install path from '" + installPath + "'.",
+                                    e.ToString());
+                }
             }
         }
 